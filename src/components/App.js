7f69bd8d1f341671c2337e9758d1bf9ec27f3eaf
yaxpaccruzjuarez <ypjuarez11@gmail.com>
2022-05-25 11:45:28 -0400

    commit tree 53b96b976825142a675488b623c33806616217db

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+const [listings, setListings] = useState()
+
+useEffect(() => {
+  fetch("/listings")
+  .then((r) => r.json())
+  .then(data => setListings(data));
+}, [])
+
+console.log(listings)
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} />
     </div>
   );
 }
